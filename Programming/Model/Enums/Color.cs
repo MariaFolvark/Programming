2bf10b849a2d6d4105a84316a282686743647c6e
MariaFolvark <FolvarochnaiaM@yandex.ru>
2026-02-26 18:17:17 +0700

    Добавлены перечисления

---
+++ Programming/Model/Enums/Color.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model.Enums
+{
+    public enum Color
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        IceBlue,
+        Blue,
+        Purple
+    }
+}
