5571c5a0d2ede668f8782c06b9acf42306791f56
MariaFolvark <FolvarochnaiaM@yandex.ru>
2026-04-06 21:00:22 +0700

    Изменены св-ва Name и Surname класса Contact: добавлен метод AssertStringContainsOnlyLetters

--- Programming/Model/Contact.cs
+++ Programming/Model/Contact.cs
@@ -12,8 +12,20 @@ namespace Programming.Model
         private string surname = "Undefined";
         private string phone_number = "Undefined";
 
-        public string Name { get { return name; } set { name = value; } }
-        public string Surname { get {  return surname; } set {  surname = value; } }
+        public string Name {
+            get { return name; }
+            set
+            {
+                name = AssertStringContainsOnlyLetters(value, "name");
+            }
+        }
+        public string Surname {
+            get {  return surname; }
+            set
+            {
+                surname = AssertStringContainsOnlyLetters(value, "surname");
+            }
+        }
         public string PhoneNumber { get { return phone_number; } set { phone_number = value; } }
         public Contact(string name, string surname, string phone_number)
         {
@@ -22,5 +34,37 @@ namespace Programming.Model
             this.PhoneNumber = phone_number;
         }
         public Contact() { }
-    }
+
+        private string AssertStringContainsOnlyLetters(string value, string name)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Строка не может быть пустой.");
+            }
+
+            foreach (char c in value)
+            {
+                if (!char.IsLetter(c))
+                {
+                    throw new ArgumentException($"Переменная {name} должна содержать только буквы английского алфавита.");
+                }
+            }
+
+            return value;
+        }
+
+    }    
 }
+
+/*
+try
+{
+    this.Name = name;
+    this.Surname = surname;
+    this.PhoneNumber = phone_number;
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка при создании контакта: {ex.Message}");
+}
+ */
